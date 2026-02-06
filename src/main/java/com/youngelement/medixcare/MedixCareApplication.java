a850b78f2e8552c9e1c80cfee553f08abfc170b6
Ajay Mali <work.ajay27@gmail.com>
2026-02-06 17:12:14 +0530

    Initilize Spring boot project and Added Depencencies;

---
+++ src/main/java/com/youngelement/medixcare/MedixCareApplication.java
@@ -0,0 +1,13 @@
+package com.youngelement.medixcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedixCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedixCareApplication.class, args);
+	}
+
+}
